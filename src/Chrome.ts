b2547331161c2238985104bf7e2d2b8798a6d54e
Joel Griffith <joel@joelgriffith.net>
2018-01-28 08:12:51 -0800

    Better updates to dev command, removing unused packages

--- src/Chrome.ts
+++ src/Chrome.ts
@@ -7,7 +7,12 @@ import * as multer from 'multer';
 import * as vm from 'vm';
 import { launch } from 'puppeteer';
 
-const chromeId = () => {
+const debug = require('debug')('browserless/chrome');
+const queue = require('queue');
+const version = require('../version.json');
+const protocol = require('../protocol.json');
+
+const chromeTarget = () => {
   var text = '';
   var possible = 'ABCDEF0123456789';
 
@@ -15,43 +20,25 @@ const chromeId = () => {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
 
-  return text;
+  return `/devtools/page/${text}`;
 };
-
-const debug = require('debug')('browserless/chrome');
-const queue = require('queue');
-const version = require('../version.json');
-const protocol = require('../protocol.json');
 
 const asyncMiddleware = (handler) => {
   return (req, socket, head) => {
     Promise.resolve(handler(req, socket, head))
       .catch((error) => {
-        console.warn(error);
+        debug(`ERROR: ${error}`);
         socket.write('HTTP/1.1 429 Too Many Requests\r\n');
         socket.end();
       });
   }
-}
+};
 
 export interface opts {
-  // How long each session has before closing
   connectionTimeout: number;
-
-  // How long each debug session has before closing
-  debugConnectionTimeout: number;
-
-  // The port to expose for incoming requests (both http and ws)
   port: number;
-
-  // Maximum number of concurrent sessions
   maxConcurrentSessions: number;
-
-  // Maximum number to queue before 429'ing/reject requests
   maxQueueLength: number;
-
-  // Logs activity every 5 seconds
-  logActivity: boolean;
 }
 
 interface chrome {
@@ -65,7 +52,6 @@ export class Chrome {
   public maxConcurrentSessions: number;
   public maxQueueLength: number;
   public connectionTimeout: number;
-  public debugConnectionTimeout: number;
 
   private proxy: any;
   private queue: any[];
@@ -77,13 +63,11 @@ export class Chrome {
     this.maxConcurrentSessions = opts.maxConcurrentSessions;
     this.maxQueueLength = opts.maxQueueLength;
     this.connectionTimeout = opts.connectionTimeout;
-    this.debugConnectionTimeout = opts.debugConnectionTimeout;
 
     this.debuggerScripts = new Map();
 
     this.proxy = new httpProxy.createProxyServer();
     this.proxy.on('error', function (err, _req, res) {
-      console.log('ERR: ' + err);
       if (res.writeHead) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
       }
@@ -91,6 +75,7 @@ export class Chrome {
       if (res.close) {
         res.close();
       }
+
       debug(`Issue communicating with Chrome: "${err.message}"`);
       res.end(`Issue communicating with Chrome`);
     });
@@ -103,28 +88,17 @@ export class Chrome {
 
     debug({
       maxConcurrentSessions: opts.maxConcurrentSessions,
-      maxQueueLenth: opts.maxQueueLength,
+      maxQueueLength: opts.maxQueueLength,
       connectionTimeout: opts.connectionTimeout,
-      debugConnectionTimeout: opts.debugConnectionTimeout,
       port: opts.port,
     }, `Final Options`);
-
-    opts.logActivity && setInterval(() => {
-      debug({
-        queue: this.queue.length,
-      });
-    }, 5000);
   }
 
   private async launchChrome({ flags, opts }): Promise<chrome> {
-    const start = Date.now();
-    const args = flags.concat('--no-sandbox');
-
-    return launch({ ...opts, args })
-      .then((chrome) => {
-        debug(`${chrome.wsEndpoint()}: Chrome launched in ${Date.now() - start}`);
-        return chrome;
-      })
+    return launch({
+      ...opts,
+      args: flags.concat('--no-sandbox'),
+    })
       .catch((error) => console.error(error));
   }
 
@@ -132,14 +106,20 @@ export class Chrome {
     const app = express();
     const upload = multer();
 
+    app.use('/', express.static('public'));
+    app.get('/json/version', (_req, res) => res.json(version));
+    app.get('/json/protocol', (_req, res) => res.json(protocol));
+
     app.post('/execute', upload.single('file'), async (req, res) => {
-      const targetId = `/devtools/page/${chromeId()}`;
+      const targetId = chromeTarget();
       const code = `
       (async() => {
         ${req.file.buffer.toString().replace('debugger', 'page.evaluate(() => { debugger; })')}
       })().catch((error) => {
         console.error('Puppeteer Runtime Error:', error.stack);
       });`;
+
+      debug(`/execute: Script uploaded\n${code}`);
 
       this.debuggerScripts.set(targetId, code);
 
@@ -149,50 +129,36 @@ export class Chrome {
       });
     });
 
-    app.get('/json/version', (_req, res) => {
-      res.json(version);
-    });
+    app.get('/json*', asyncMiddleware(async (req, res) => {
+      const targetId = chromeTarget();
+      const baseUrl = req.get('host');
+      const protocol = req.protocol.includes('s') ? 'wss': 'ws';
 
-    app.get('/json/protocol', (_req, res) => {
-      res.json(protocol);
-    });
+      debug(`${req.url}: JSON protocol request.`);
 
-    // app.get('/json*', asyncMiddleware(async (req, res) => {
-    //   const sessionId = uuid();
-    //   const removeFromQueue = () => this.removeFromQueue(sessionId);
-
-    //   req.on('end', removeFromQueue);
-
-    //   const { targetId } = await this.startChromeSession({
-    //     flags: [],
-    //     opts: {},
-    //     createNewPage: true,
-    //     sessionId,
-    //     timeout: this.connectionTimeout,
-    //   });
-
-    //   req.removeListener('end', removeFromQueue);
-
-    //   const baseUrl = req.get('host');
-    //   const protocol = req.protocol.includes('s') ? 'wss': 'ws';
-
-    //   res.json([{
-    //     targetId,
-    //     description: '',
-    //     devtoolsFrontendUrl: `/devtools/inspector.html?${protocol}=${baseUrl}${targetId}`,
-    //     title: 'about:blank',
-    //     type: 'page',
-    //     url: 'about:blank',
-    //     webSocketDebuggerUrl: `${protocol}://${baseUrl}${targetId}`
-    //  }]);
-    // }));
-
-    app.use('/', express.static('public'));
+      res.json([{
+        targetId,
+        description: '',
+        devtoolsFrontendUrl: `/devtools/inspector.html?${protocol}=${baseUrl}${targetId}`,
+        title: 'about:blank',
+        type: 'page',
+        url: 'about:blank',
+        webSocketDebuggerUrl: `${protocol}://${baseUrl}${targetId}`
+     }]);
+    }));
 
     return this.server = http
       .createServer(app)
       .on('upgrade', asyncMiddleware(async(req, socket, head) => {
-        debug(`WebSocket connection upgrade request, queue: ${this.queue.length}`);
+        debug(`${req.url}: Inbound WebSocket request, ${this.queue.length} in queue.`);
+
+        if (this.queue.length >= this.maxQueueLength) {
+          debug(`${req.url}: Queue is full, rejecting`);
+          socket.write('HTTP/1.1 429 Too Many Requests\r\n');
+          socket.end();
+          return;
+        }
+
         this.queue.push((done) => {
           const parsedUrl = url.parse(req.url, true);
           const route = parsedUrl.pathname || '/';
@@ -202,20 +168,36 @@ export class Chrome {
             .map((value, key) => `${key}${value ? `=${value}` : ''}`)
             .value();
 
-          // Strip qs params as it causes chrome to choke
-          req.url = route;
-
-          socket.on('close', done);
+          debug(`${req.url}: Launching Chrome for WebSocket upgrade.`);
 
           this.launchChrome({ flags, opts: {} })
             .then(async (chrome) => {
               const browserWsEndpoint = chrome.wsEndpoint();
 
-              if (this.debuggerScripts.has(route)) {
-                const page = await chrome.newPage();
-                const port = url.parse(browserWsEndpoint).port;
-                req.url = `/devtools/page/${page._target._targetId}`;
+              debug(`${req.url}: Chrome Launched.`);
 
+              socket.on('close', () => {
+                debug(`${req.url}: Session closed. ${this.queue.length} in queue`);
+                chrome.close();
+                done();
+              });
+
+              if (!route.includes('/devtools/page')) {
+                debug(`${req.url}: Proxying request to /devtools/browser route: ${browserWsEndpoint}.`);
+                req.url = route;
+
+                return browserWsEndpoint;
+              }
+
+              const page = await chrome.newPage();
+              const port = url.parse(browserWsEndpoint).port;
+              const pageLocation = `/devtools/page/${page._target._targetId}`;
+
+              debug(`${req.url}: Proxying request to /devtools/page route: ${pageLocation}.`);
+
+              if (this.debuggerScripts.has(route)) {
+                const code = this.debuggerScripts.get(route);
+                debug(`${req.url}: Loading prior-uploaded script to execute for route.`);
                 const scope = {
                   page,
                   console: {
@@ -225,16 +207,15 @@ export class Chrome {
                   },
                 };
 
-                vm.runInNewContext(this.debuggerScripts.get(route), scope);
-
-                return `ws://127.0.0.1:${port}`;
+                this.debuggerScripts.delete(route);
+                vm.runInNewContext(code, scope);
               }
-              return browserWsEndpoint;
+
+              req.url = pageLocation;
+
+              return `ws://127.0.0.1:${port}`;
             })
-            .then((target) => {
-              debug(`Proxying request ${req.url} to ${target}`);
-              this.proxy.ws(req, socket, head, { target });
-            });
+            .then((target) => this.proxy.ws(req, socket, head, { target }));
         });
       }))
       .listen(this.port);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import { Chrome } from './Chrome';
 import {
   connectionTimeout,
-  debugConnectionTimeout,
   port,
   maxConcurrentSessions,
   maxQueueLength,
-  logActivity,
 } from './config';
 
 new Chrome({
   connectionTimeout,
-  debugConnectionTimeout,
   port,
   maxConcurrentSessions,
   maxQueueLength,
-  logActivity,
 })
 .startServer();
