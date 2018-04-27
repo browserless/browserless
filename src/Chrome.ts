90a222073c1341f41c36a37f67b4d96a7aa538d4
Joel Griffith <joel@joelgriffith.net>
2018-04-27 11:20:10 -0700

    SIGKILL timedout sessions

--- src/Chrome.ts
+++ src/Chrome.ts
@@ -431,7 +431,7 @@ export class Chrome {
 
               socket.on('close', () => {
                 debug(`${req.url}: Session closed, stopping Chrome. ${this.queue.length} now in queue`);
-                chrome.close();
+                chrome.process().kill('SIGKILL');
                 done();
               });
 
