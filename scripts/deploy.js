f8b9dc2bd660c0ce07c5d11d529cb67e7b7acf28
Joel Griffith <joel@joelgriffith.net>
2018-05-11 22:41:03 -0700

    Please don't warn

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -30,7 +30,7 @@ const deployPuppeteerVersion = async (version) => {
   await logExec(`git checkout puppeteer-${version} --quiet`);
   await logExec(`git merge ${DEPLOY_BRANCH} --commit --quiet`);
   await logExec(`rm -rf node_modules package-lock.json`);
-  await logExec(`npm install`)
+  await logExec(`npm install --silent`);
   await logExec(`npm install --silent --save --save-exact puppeteer@${version}`);
   await getMeta();
 
