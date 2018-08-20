49fab64395c38bf8e604374846bdb29f4c082caa
Joel Griffith <joel@joelgriffith.net>
2018-08-20 08:53:59 -0700

    Working selenium (WIP)

--- src/browserless-server.ts
+++ src/browserless-server.ts
@@ -1,4 +1,5 @@
 import * as bodyParser from 'body-parser';
+import * as chromedriver from 'chromedriver';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as fs from 'fs';
@@ -7,7 +8,6 @@ import * as httpProxy from 'http-proxy';
 import * as _ from 'lodash';
 import * as path from 'path';
 import { setInterval } from 'timers';
-import * as url from 'url';
 
 import {
   asyncMiddleware,
@@ -27,6 +27,13 @@ import {
 import { ChromeService } from './chrome-service';
 import { ResourceMonitor } from './hardware-monitoring';
 import { IBrowserlessOptions } from './models/options.interface';
+
+const PORT = 9515;
+chromedriver.start([
+  '--url-base=wd/hub',
+  `--port=${PORT}`,
+  '--verbose',
+]);
 
 const debug = getDebug('server');
 
@@ -238,8 +245,12 @@ export class BrowserlessServer {
       return this.server = http
         .createServer(async (req, res) => {
           // Handle selenium requests
-          if (req.url && req.url.includes('/wd/hub/')) {
-            return this.handleWebdriverSession(req, res);
+          if (req.url && req.url.includes('/wd/hub/session')) {
+            debug(`Inbound webdriver request`);
+
+            return this.proxy.web(req, res, { target: `http://127.0.0.1:${9515}` }, (e) => {
+              debug(`Issue in webdriver: ${e.message}`);
+            });
           }
 
           return app(req, res);
@@ -247,25 +258,6 @@ export class BrowserlessServer {
         .on('upgrade', asyncMiddleware(this.chromeService.runWebSocket.bind(this.chromeService)))
         .listen(this.config.port, this.config.host, resolve);
     });
-  }
-
-  public async handleWebdriverSession(req, res) {
-    const path = url.parse(req.url);
-    const method = req.method;
-
-    if (method.toLowerCase() === 'post') {
-      if (path.pathname === '/wd/hub/session') {
-        debug(`${req.url}: Web-driver create request.`);
-        return this.chromeService.startWebDriver(req, res);
-      }
-      debug(`${req.url}: Web-driver existing-session request.`);
-      const prior = this.chromeService.webDriver.get('SHOULD_BE_STATIC');
-      console.log(prior.port);
-      return this.proxy.web(req, res, { target: `http://localhost:${prior.port}` });
-    }
-
-    res.write(`Unknown command: ${method}: ${path}`);
-    return res.end();
   }
 
   public async close() {
