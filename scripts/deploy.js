259e085a399b0048b7407a97d4aa39b059d723b8
Joel Griffith <joel@joelgriffith.net>
2019-03-07 16:08:47 -0800

    Work on deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,25 +3,14 @@ const child = require('child_process');
 const util = require('util');
 const debug = require('debug')('browserless-docker-deploy');
 const exec = util.promisify(child.exec);
-const {
-  flatMap,
-  noop
-} = require('lodash');
+const { flatMap } = require('lodash');
 
 const {
   chromeVersions,
   puppeteerVersions,
-  version,
 } = require('../package.json');
 
-const DEPLOY_BRANCH = 'master';
-const metaFiles = [
-  'package.json',
-  'package-lock.json',
-  'version.json',
-  'protocol.json',
-  'hints.json'
-];
+const REPO = 'browserless/chrome'
 
 const logExec = (cmd) => {
   debug(`  "${cmd}"`);
@@ -33,75 +22,43 @@ const logExec = (cmd) => {
   });
 };
 
-async function checkoutReleaseBranch () {
-  return logExec(`git checkout ${DEPLOY_BRANCH} --quiet`);
+async function cleanup () {
+  return logExec(`git reset HEAD --hard`);
 }
 
-const deployVersion = async (branch, chromeVersion) => {
-  const version = puppeteerVersions[chromeVersion];
+// version is the full tag (1.2.3-puppeteer-1.11.1)
+// chrome version is one of the versions in packageJson.chromeVersions
+const deployVersion = async (tagVersion, chromeVersion) => {
+  const puppeteerVersion = puppeteerVersions[chromeVersion];
 
-  debug(`${branch}: Deploying release of browserless, puppeteer@${version}`);
+  debug(`${tagVersion}: Beginning docker build and publish`);
 
-  const currentBranch = await logExec('git rev-parse --abbrev-ref HEAD');
+  await logExec(`npm install --silent --save --save-exact puppeteer@${puppeteerVersion}`);
+  await logExec(`npm run meta --silent ${tagVersion.includes('chrome-stable') ? '-- --chrome-stable' : ''}`);
 
-  if (currentBranch !== DEPLOY_BRANCH) {
-    await checkoutReleaseBranch();
-  }
+  const versionJson = require('../version.json');
+  const chromeStableArg = tagVersion.includes('chrome-stable') ? 'true' : 'false';
 
-  await logExec(`git checkout -b ${branch} --quiet`);
-  await logExec(`npm install --silent --save --save-exact puppeteer@${version}`);
-  await logExec(`npm run meta --silent ${version.includes('chrome-stable') ? '-- --chrome-stable' : ''}`);
+  // docker build
+  await logExec(`docker build \
+  --build-arg "USE_CHROME_STABLE=${chromeStableArg}" \
+  --label "browser=${versionJson.Browser} \
+  --label "protocolVersion=${versionJson['Protocol-Version']} \
+  --label "v8Version=${versionJson['V8-Version']} \
+  --label "webkitVersion=${versionJson['WebKit-Version']} \
+  --label "debuggerVersion=${versionJson.Browser['Debugger-Version']} \
+  --label "puppeteerVersion=${versionJson.Browser['Puppeteer-Version']} \
+  -t ${REPO}:${tagVersion} .`);
 
-  for (let file of metaFiles) {
-    try {
-      await logExec(`git status --porcelain | grep ${file}`);
-      debug(`${branch}: Changes found in Puppeteer@${version}, committing file ${file}`);
-      await logExec(`git add ${file}`);
-      await logExec(`git commit --quiet -m "DEPLOY.JS: Updating ${file} browser meta output" ${file}`);
-    } catch (err) {
-      debug(`${branch}: No meta changes found, proceeding to next version.`);
-    }
-  }
+  // docker push
+  await logExec(`docker push ${REPO}:${tagVersion}`);
 
-  // Have to do `&> /dev/null` to avoid remote messages
-  await logExec(`git push origin ${branch} --force --quiet --no-verify &> /dev/null`);
-}
-
-async function deleteBranches(branches) {
-  return Promise.all(
-    branches.map((branch) => {
-      console.log(`Deleting branch ${branch}`);
-      return exec(`git branch -D ${branch}`).catch(noop);
-    })
-  );
+  // git reset for next update
+  await cleanup();
 }
 
 async function deploy () {
-  const branch = await logExec('git rev-parse --abbrev-ref HEAD');
-
-  if (branch.trim() !== DEPLOY_BRANCH) {
-    console.error(`Not on deploy branch "${DEPLOY_BRANCH}" branch, exiting`);
-    process.exit(1);
-  }
-
-  const status = await logExec('git status --porcelain');
-
-  if (status.length) {
-    console.error('Un-tracked files in git, please commit before deploying.');
-    process.exit(1);
-  }
-
-  const preCheckBranch = `${version}-chrome-stable`;
-  const versionExists = await logExec(`git ls-remote --heads https://github.com/joelgriffith/browserless.git ${preCheckBranch}`);
-
-  if (versionExists.trim().length) {
-    console.log(`Version ${version} already exists on GitHub. Did you forget to 'npm bump'?`);
-    process.exit(1);
-  }
-
-  debug(`On branch ${DEPLOY_BRANCH} and no un-tracked files in git, proceeding to build deployment.`);
-
-  const branches = flatMap(chromeVersions, (chromeVersion) => {
+  const versions = flatMap(chromeVersions, (chromeVersion) => {
     const [ major, minor, patch ] = version.split('.');
 
     const patchBranch = `${major}.${minor}.${patch}-${chromeVersion}`;
@@ -115,14 +72,18 @@ async function deploy () {
     ];
   });
 
-  await deleteBranches(branches.map(([ branch ]) => branch));
+  await versions.reduce(
+    (lastJob, [version, chromeVersion]) =>
+      lastJob
+        .then(() => deployVersion(version, chromeVersion))
+        .catch((error) => {
+          console.log(`Error in build (${version}): `, error);
+          process.exit(1);
+        }),
+    Promise.resolve()
+  );
 
-  await branches.reduce((lastJob, [branch, chromeVersion]) =>
-    lastJob.then(() => deployVersion(branch, chromeVersion)), Promise.resolve());
-
-  debug(`Checking out master and removing release branches.`);
-
-  await checkoutReleaseBranch();
+  debug(`Complete! Cleaning up file-system and exiting.`);
 }
 
 deploy();
