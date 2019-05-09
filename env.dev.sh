b4120dd5b95c8fbea405ecddba2466da7cf5b6ea
Cristian Gabbanini <cristian@begenius.it>
2019-05-09 14:58:05 +0200

    Support cross-env environment variables
    Replace the source command with env-cmd and rename/update the env.dev.sh
    accordingly

--- env.dev.sh
+++
@@ -1,7 +0,0 @@
-export ENABLE_CORS=true
-export MAX_CONCURRENT_SESSIONS=10
-export FUNCTION_BUILT_INS='["*"]'
-export DEBUG=browserless*
-export FUNCTION_EXTERNALS=true
-export PORT=3000
-export CONNECTION_TIMEOUT=-1
