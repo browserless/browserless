d19fa0fa1d69d17639831319d9d71ef58ea7abfe
Kyle Smith <knksmith57@gmail.com>
2019-09-04 08:27:15 -0500

    feat: support custom lighthouse audit configuration on /stats (#325)
    Support optional config context property on /stats to allow
    customization of lighthouse run. Fallback to lighthouse:default
    (existing behavior) if config is not provided.
    
    relates to #317, knksmith57/browserless-lighthouse#2

--- functions/stats.js
+++ functions/stats.js
@@ -2,14 +2,18 @@ const lighthouse = require('lighthouse');
 const { URL } = require('url');
 const { canLog } = require('./build/utils');
 
+const DEFAULT_AUDIT_CONFIG = {
+  extends: 'lighthouse:default'
+}
+
 module.exports = async ({ browser, context }) => {
-  const { url } = context;
+  const { url, config = DEFAULT_AUDIT_CONFIG } = context;
 
   const { lhr } = await lighthouse(url, {
     port: (new URL(browser.wsEndpoint())).port,
     output: 'json',
     logLevel: canLog ? 'info' : 'silent',
-  });
+  }, config);
 
   return {
     data: lhr,
