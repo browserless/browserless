e4a02db0246e04c36e9ac29626787e8e1e766e73
Joel Griffith <joel@joelgriffith.net>
2020-03-30 13:33:13 -0700

    Use 99 for display

--- start.sh
+++ start.sh
@@ -10,10 +10,10 @@ _kill_procs() {
 # Relay quit commands to processes
 trap _kill_procs SIGTERM SIGINT
 
-Xvfb :1 -screen 0 1024x768x16 -nolisten tcp -nolisten unix &
+Xvfb :99 -screen 0 1024x768x16 -nolisten tcp -nolisten unix &
 xvfb=$!
 
-export DISPLAY=:1
+export DISPLAY=:99
 
 dumb-init -- node ./build/index.js $@ &
 node=$!
