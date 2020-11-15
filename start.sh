9bfa065d05b384b5b6f9b9941939d95670c7ee5e
Robbert Langezaal <opicron@users.noreply.github.com>
2020-11-15 12:56:13 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 # When docker restarts, this file is still there,
 # so we need to kill it just in case
-[ -f /tmp/.X99-lock ] && rm /tmp/.X99-lock
+[ -f /tmp/.X99-lock ] && rm -f /tmp/.X99-lock
 
 _kill_procs() {
   kill -TERM $node
