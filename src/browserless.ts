79611cc1286a34fa06c99f68611b1b50fb00f0b5
Joel Griffith <joel@joelgriffith.net>
2021-02-22 13:58:19 -0800

    Fix test marking unauth requests as rejected

--- src/browserless.ts
+++ src/browserless.ts
@@ -326,7 +326,6 @@ export class BrowserlessServer {
             return this.rejectSocket({
               header: `HTTP/1.1 403 Forbidden`,
               message: `Forbidden`,
-              metricType: 'rejected',
               req: reqParsed,
               socket,
             });
