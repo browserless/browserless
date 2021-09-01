2b466704e957907a365c323556dbda504ef24f74
Joel Griffith <joel@joelgriffith.net>
2021-09-01 10:12:18 -0700

    Fix issues with images not loading in HTML-only PDF/screenshots plus bump chrome rev

--- functions/pdf.js
+++ functions/pdf.js
@@ -138,17 +138,7 @@ module.exports = async function pdf({ page, context }) {
   if (url !== null) {
     response = await page.goto(url, gotoOptions);
   } else {
-    // Whilst there is no way of waiting for all requests to finish with setContent,
-    // you can simulate a webrequest this way
-    // see issue for more details: https://github.com/GoogleChrome/puppeteer/issues/728
-
-    await page.setRequestInterception(true);
-    page.once('request', (request) => {
-      request.respond({ body: html });
-      page.on('request', (request) => request.continue());
-    });
-
-    response = await page.goto('http://localhost', gotoOptions);
+    response = await page.setContent(html, gotoOptions);
   }
 
   if (addStyleTag.length) {
