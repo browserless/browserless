1c51e9af7fadeab5f079680ca8d3caa5bc51aee0
Joel Griffith <joel@joelgriffith.net>
2022-06-22 13:57:23 -0700

    Move to zx for deploy scripting

--- scripts/deploy-base.js
+++ scripts/deploy-base.js
@@ -1,10 +1,6 @@
-#!/usr/bin/env node
+#!/usr/bin/env zx
 /* eslint-disable no-undef */
-const child = require('child_process');
-const util = require('util');
-
 const debug = require('debug')('browserless-docker-deploy');
-const exec = util.promisify(child.exec);
 
 const BASE = 'browserless/base';
 const TARGET_ARCH = ['linux/amd64', 'linux/arm64/v8'];
@@ -12,26 +8,14 @@ const VERSION = process.env.VERSION;
 
 if (!VERSION) {
   throw new Error(
-    `Expected a $VERSION env variable to tag the ${BASE} repo, but none was found.`,
+    `Expected a $VERSION env variable to tag the ${BASE} repo, but none was found.`
   );
 }
 
-const logExec = (cmd) => {
-  debug(`  "${cmd}"`);
-  return exec(cmd).then(({ stdout, stderr }) => {
-    if (stderr.trim().length > 0) {
-      console.warn(stderr.slice(-500));
-    }
-    return stdout.trim();
-  });
-};
-
 const buildBase = async () => {
-  await logExec(
-    `docker buildx build --push --platform ${TARGET_ARCH.join(
-      ',',
-    )} -t ${BASE}:latest -t ${BASE}:${VERSION} ./base`,
-  );
+  await $`docker buildx build --push --platform ${TARGET_ARCH.join(
+    ','
+  )} -t ${BASE}:latest -t ${BASE}:${VERSION} ./base`;
 };
 
 (async function deploy() {
