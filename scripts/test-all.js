a60ecc8b8cda2169e3cc93b79a4578a1633460e9
Joel Griffith <joel@joelgriffith.net>
2022-12-02 18:44:41 -0800

    Logs on test-all

--- scripts/test-all.js
+++ scripts/test-all.js
@@ -12,17 +12,24 @@ const { releaseVersions, chromeVersions } = require('../package.json');
 
   for (version of versions) {
     const chromeVersion = chromeVersions[`puppeteer-${version}`].chromeRevision;
-    await $`npm install --silent --save --save-exact puppeteer@${version} && npm run build`;
-    await $`docker buildx build \
-    --load \
-    --platform linux/amd64 \
-    --build-arg "BASE_VERSION=latest" \
-    --build-arg "USE_CHROME_STABLE=false" \
-    --build-arg "PUPPETEER_CHROMIUM_REVISION=${chromeVersion}" \
-    --build-arg "PUPPETEER_VERSION=${version}" \
-    --build-arg "USE_CHROME_STABLE=false" \
-    -t browserless/chrome:${version} \
-    .`;
-    await $`docker run --ipc=host -e CI=true --entrypoint ./test-all.sh browserless/chrome:${version}`;
+
+    try {
+      await $`docker buildx build \
+      --load \
+      --platform linux/amd64 \
+      --build-arg "BASE_VERSION=latest" \
+      --build-arg "USE_CHROME_STABLE=false" \
+      --build-arg "PUPPETEER_CHROMIUM_REVISION=${chromeVersion}" \
+      --build-arg "PUPPETEER_VERSION=${version}" \
+      --build-arg "USE_CHROME_STABLE=false" \
+      -t browserless/chrome:${version} \
+      .`;
+      await $`docker run --ipc=host -e CI=true --entrypoint ./test-all.sh browserless/chrome:${version}`;
+    } catch (err) {
+      console.error(`Error running tests for ${version} of puppeteer: ${err.message}`);
+      process.exit(1);
+    }
+    console.log(`Successfully ran all tests!`);
+    process.exit(0);
   }
 })();
