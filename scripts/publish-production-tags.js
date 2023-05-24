375da815e3377d206042d600b1d9e2551f789d98
Joel Griffith <joel@joelgriffith.net>
2023-05-24 09:47:03 -0700

    Making script interactive

--- scripts/publish-production-tags.js
+++ scripts/publish-production-tags.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env zx
 
 /* eslint-disable no-undef */
-const { createInterface } = require('readline');
-
 const { map } = require('lodash');
 const argv = require('yargs').argv;
 
@@ -14,147 +12,113 @@ const {
 
 if (argv.h || argv.help) {
   return console.log(`---
-Builds production tags of the browserless/chrome image with options. You can combine these as you wish for complete control.
-
-DEFAULT:
 '$ npm run deploy'
 
-Builds a new browserless/base with a version defined in the package.json field and builds then pushes the image into docker.
-
-SET A RELEASE VERSION VERSION:
-'$ VERSION=1.2.3 npm run deploy'
-
-Override the package.json version, used in the first part of our version semantics. Builds the base and chrome repos, pushing after success.
-
-SKIP BASE BUILDS:
-'$ npm run deploy -- --skipBase'
-
-Skips building the base layer. Note that you'll still need a browserless/base:$VERSION available.
-
-SPECIFY A PUPPETEER VERSION:
-'$ npm run deploy -- --versions=puppeteer-1.20.0,puppeteer-20.2.1'
-
-Releases only a specific version, and has to be included in the 'releaseVersions' property of the package.json file. Comma-separated lists only.
-
-SPECIFY A SPECIFIC PLATFORM:
-'$ npm run deploy -- --platform=linux/arm64'
-
-Releases only a specific platform for each release version. Supports linux/arm64 and linux/amd64 platforms at the moment. If a prior tag has
-been made this *will* override it possibly removing the prior platform.
+Builds a new browserless/base and browserless/chrome with a version defined in the package.json field and builds then pushes the image into docker.
+This CLI is interactive and you can override many parts of this build process. Please follow the prompts which include sensible defaults.
 ---`);
 }
 
-const version = process.env.VERSION ?? npmVersion;
-const buildBase = !argv.skipBase;
-const requestedPlatform = argv.platform;
-const requestedVersions = argv.versions
-  ? argv.versions.split(',')
-  : releaseVersions;
-const missingVersions = requestedVersions.filter(
-  (v) => !releaseVersions.includes(v),
-);
+(async function deploy() {
+  const baseRepo =
+    (await question(
+      `Enter a custom base repo, or use default of "browserless/base"? `,
+    )) || 'browserless/base';
+  const repo =
+    (await question(
+      `Enter a custom repo, or use default of "browserless/chrome"? `,
+    )) || 'browserless/chrome';
+  const version =
+    (await question(
+      `Enter a semantic version (eg "1.5.3") or use default of "${npmVersion}"? `,
+    )) || npmVersion;
+  const buildBase = (
+    (await question(`Build the the "${baseRepo}" image (yes/no)? `)) || 'no'
+  ).includes('y');
+  const platforms =
+    (await question(
+      `Which platforms do you want to build for (default is "linux/arm64,linux/amd64")? `,
+    )) || 'linux/arm64,linux/amd64';
+  const puppeteerVersions =
+    (await question(
+      `Which puppeteer versions do you want to make (Must be contained package.json "releaseVersions" and defaults to that list)? `,
+    )) || releaseVersions.join(',');
 
-// Validate arg parsing
-if (missingVersions.length) {
-  throw new Error(
-    `Versions: ${missingVersions.join(
-      ', ',
-    )} are missing from the package.json file manifest. Please double check your versions`,
+  const requestedVersions = puppeteerVersions.split(',');
+
+  const missingVersions = requestedVersions.filter(
+    (v) => !releaseVersions.includes(v),
   );
-}
 
-if (
-  requestedPlatform &&
-  !['linux/amd64', 'linux/arm64'].includes(requestedPlatform)
-) {
-  console.warn(
-    `Unsupported --platform switch. 'linux/amd64' or 'linux/arm64' only are supported.`,
-  );
-}
-
-const prompt = (question) => {
-  const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  return new Promise((resolve) =>
-    rl.question(question, (ans) => {
-      rl.close();
-      resolve(ans.trim());
-    }),
-  );
-};
-
-// LOG...
-const work = [
-  buildBase
-    ? `Building ""browserless/base:${version}" on platform ${
-        requestedPlatform ?? 'all platforms'
-      }`
-    : null,
-  `Building ${requestedVersions.join(', ')} on platform ${
-    requestedPlatform ?? 'all platforms'
-  }`,
-];
-
-console.log(work.filter((_) => !!_).join('.\n'));
-
-const deployVersion = async (tags, v) => {
-  const versionInfo = chromeVersions[v];
-
-  if (!versionInfo) {
+  // Validate arg parsing
+  if (missingVersions.length) {
     throw new Error(
-      `Couldn't locate version info for puppeteer version ${v}. Did you forget to add it to the package.json?`,
+      `Versions: ${missingVersions.join(
+        ', ',
+      )} are missing from the package.json file manifest. Please double check your versions`,
     );
   }
 
-  const puppeteerVersion = versionInfo.puppeteer;
-  const puppeteerChromiumRevision = versionInfo.chromeRevision;
-  const platforms = requestedPlatform
-    ? [requestedPlatform]
-    : versionInfo.platforms || ['linux/amd64'];
+  // LOG...
+  const work = [
+    buildBase ? `Building "${baseRepo}:${version}" on "${platforms}"` : null,
+    `Building "${repo}:${version}" on "${platforms}" for versions ${requestedVersions.join(
+      ', ',
+    )}`,
+  ];
 
-  const [patchBranch, minorBranch, majorBranch] = tags;
-  const isChromeStable = majorBranch.includes('chrome-stable');
+  console.log(work.filter((_) => !!_).join('.\n'));
 
-  process.env.PUPPETEER_CHROMIUM_REVISION = puppeteerChromiumRevision;
-  process.env.USE_CHROME_STABLE = false;
-  process.env.CHROMEDRIVER_SKIP_DOWNLOAD = true;
-  process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = true;
+  const proceed = (await question('Proceed (y/n)?')).includes('y');
 
-  if (isChromeStable) {
-    process.env.USE_CHROME_STABLE = true;
-    process.env.CHROMEDRIVER_SKIP_DOWNLOAD = false;
-  }
-
-  const chromeStableArg = isChromeStable ? 'true' : 'false';
-
-  // Since we load the image, we can't run in parallel as it's
-  // a known issue atm.
-  await $`docker buildx build \
-  --push \
-  --platform ${platforms.join(',')} \
-  --build-arg "BASE_VERSION=${version}" \
-  --build-arg "PUPPETEER_CHROMIUM_REVISION=${puppeteerChromiumRevision}" \
-  --build-arg "USE_CHROME_STABLE=${chromeStableArg}" \
-  --build-arg "PUPPETEER_VERSION=${puppeteerVersion}" \
-  -t browserless/chrome:${patchBranch} \
-  -t browserless/chrome:${minorBranch} \
-  -t browserless/chrome:${majorBranch} .`;
-};
-
-(async function deploy() {
-  // Wait 30 seconds before proceeding to allow folks to verify.
-  const answer = await prompt('\nProceed (y/n)?');
-
-  if (answer !== 'y' && answer !== 'yes') {
+  if (!proceed) {
     return;
   }
 
+  const deployVersion = async (tags, v) => {
+    const versionInfo = chromeVersions[v];
+
+    if (!versionInfo) {
+      throw new Error(
+        `Couldn't locate version info for puppeteer version ${v}. Did you forget to add it to the package.json?`,
+      );
+    }
+
+    const puppeteerVersion = versionInfo.puppeteer;
+    const puppeteerChromiumRevision = versionInfo.chromeRevision;
+
+    const [patchBranch, minorBranch, majorBranch] = tags;
+    const isChromeStable = majorBranch.includes('chrome-stable');
+
+    process.env.PUPPETEER_CHROMIUM_REVISION = puppeteerChromiumRevision;
+    process.env.USE_CHROME_STABLE = false;
+    process.env.CHROMEDRIVER_SKIP_DOWNLOAD = true;
+    process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = true;
+
+    if (isChromeStable) {
+      process.env.USE_CHROME_STABLE = true;
+      process.env.CHROMEDRIVER_SKIP_DOWNLOAD = false;
+    }
+
+    const chromeStableArg = isChromeStable ? 'true' : 'false';
+
+    // Since we load the image, we can't run in parallel as it's
+    // a known issue atm.
+    await $`docker buildx build \
+    --push \
+    --platform ${platforms} \
+    --build-arg "BASE_VERSION=${version}" \
+    --build-arg "BASE_REPO=${baseRepo}" \
+    --build-arg "PUPPETEER_CHROMIUM_REVISION=${puppeteerChromiumRevision}" \
+    --build-arg "USE_CHROME_STABLE=${chromeStableArg}" \
+    --build-arg "PUPPETEER_VERSION=${puppeteerVersion}" \
+    -t ${repo}:${patchBranch} \
+    -t ${repo}:${minorBranch} \
+    -t ${repo}:${majorBranch} .`;
+  };
+
   if (buildBase) {
-    const basePlatform = requestedPlatform ?? 'linux/amd64,linux/arm64';
-    await $`docker buildx build --push --platform ${basePlatform} -t browserless/base:${version} base`;
+    await $`docker buildx build --push --platform ${platforms} -t ${baseRepo}:${version} base`;
   }
 
   const buildVersions = map(requestedVersions, (pV) => {
