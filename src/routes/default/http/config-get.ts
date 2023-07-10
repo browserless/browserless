499ce73b22127805e62c0b9545fe1a15c88420df
Joel Griffith <joel@joelgriffith.net>
2023-07-10 11:27:19 -0500

    Update config to match v1

--- src/routes/default/http/config-get.ts
+++ src/routes/default/http/config-get.ts
@@ -12,14 +12,26 @@ import { HTTPRoute } from '../../../types.js';
 import * as util from '../../../utils.js';
 
 export interface ResponseSchema {
+  allowCORS: boolean;
+  allowFileProtocol: boolean;
+  allowGetCalls: boolean;
   concurrent: number;
   data: string;
   debug: string;
+  errorAlertURL: string | null;
+  healthFailureURL: string | null;
   host: string;
+  maxCPU: number;
+  maxMemory: number;
+  metricsJSONPath: string;
   port: number;
   queued: number;
+  queuedAlertURL: string | null;
+  rejectAlertURL: string | null;
   retries: number;
   timeout: number;
+  timeoutAlertURL: string | null;
+  token: string;
 }
 
 const route: HTTPRoute = {
@@ -39,14 +51,26 @@ const route: HTTPRoute = {
     const config = getConfig();
 
     const response: ResponseSchema = {
+      allowCORS: config.getAllowCORS(),
+      allowFileProtocol: config.getAllowFileProtocol(),
+      allowGetCalls: config.getAllowGetCalls(),
       concurrent: config.getConcurrent(),
       data: await config.getDataDir(),
       debug: config.getDebug(),
+      errorAlertURL: config.getErrorAlertURL(),
+      healthFailureURL: config.getFailedHealthURL(),
       host: config.getHost(),
+      maxCPU: config.getCPULimit(),
+      maxMemory: config.getMemoryLimit(),
+      metricsJSONPath: config.getMetricsJSONPath(),
       port: config.getPort(),
       queued: config.getQueued(),
+      queuedAlertURL: config.getQueueAlertURL(),
+      rejectAlertURL: config.getRejectAlertURL(),
       retries: config.getRetries(),
       timeout: config.getTimeout(),
+      timeoutAlertURL: config.getTimeoutAlertURL(),
+      token: config.getToken(),
     };
 
     return util.jsonResponse(res, 200, response);
