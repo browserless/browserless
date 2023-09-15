ff1351a0dcb3a2019ffde6e93cc541f0ab934528
Joel Griffith <joel@joelgriffith.net>
2023-09-15 07:57:08 -0700

    Add in test.sh and github actions for latest

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+Xvfb :1 -screen 0 1024x768x16 -nolisten tcp -nolisten unix &
+xvfb=$!
+
+export DISPLAY=:1
+
+export SOME_ENV_VAR_TO_ALLOW_IN_FUNCTIONS=true
+
+DEBUG=-* PUPPETEER_DISABLE_HEADLESS_WARNING=true ./node_modules/.bin/mocha --timeout 30000 --slow 10000 --exit $@ && kill -TERM $xvfb
