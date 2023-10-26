82f35d2fcc1cd8cba2fb9ddd6d2aebc16e165f1d
Joel Griffith <joel@joelgriffith.net>
2023-10-26 12:43:58 -0700

    Move test install up

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Install devDependencies for tests
+npm i --production=false
+
 # Setup env variables
 export DISPLAY=:1
 export DEBUG=-*
@@ -9,9 +12,6 @@ export NODE_OPTIONS="--loader ts-node/esm"
 
 Xvfb :1 -screen 0 1024x768x16 -nolisten tcp -nolisten unix &
 xvfb=$!
-
-# Install devDependencies for tests
-npm i --production=false
 
 # Run the tests
 ./node_modules/.bin/_mocha --timeout 30000 --slow 10000 --exit $@ && kill -TERM $xvfb
