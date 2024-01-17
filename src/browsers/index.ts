68907da2dab5a56a7e056b0df18298415c7d16fb
Anderson Martínez <anderson@browserless.io>
2024-01-16 21:05:28 -0400

    fix: migrate json/version route to class notation

--- src/browsers/index.ts
+++ src/browsers/index.ts
@@ -86,7 +86,7 @@ export class BrowserManager {
 
   public getVersionJSON = async () => {
     const port = await getPort();
-    const config = this.config;
+    const config = new Config();
     config.setPort(port);
 
     const browser = new CDPChromium({
@@ -233,8 +233,8 @@ export class BrowserManager {
     if (req.parsed.pathname.includes('/devtools/browser')) {
       const sessions = Array.from(this.browsers);
       const id = req.parsed.pathname.split('/').pop() as string;
-      const browser = sessions.find(
-        ([b]) => b.wsEndpoint()?.includes(req.parsed.pathname),
+      const browser = sessions.find(([b]) =>
+        b.wsEndpoint()?.includes(req.parsed.pathname),
       );
 
       if (browser) {
--- src/routes/management/http/json-version-get.ts
+++ src/routes/management/http/json-version-get.ts
@@ -1,49 +1,57 @@
 import { ServerResponse } from 'http';
 
 import {
-  contentTypes,
-  Request,
-  Methods,
-  HTTPManagementRoutes,
   APITags,
+  HTTPManagementRoutes,
+  Methods,
+  Request,
+  contentTypes,
 } from '../../../http.js';
 
 import { HTTPRoute } from '../../../types.js';
+
 import * as util from '../../../utils.js';
 
 export type ResponseSchema = {
-  "Browser": string,
-  "Debugger-Version": string,
-  "Protocol-Version": string,
-  "User-Agent": string,
-  "V8-Version": string,
-  "WebKit-Version": string,
-  "webSocketDebuggerUrl": string
+  Browser: string;
+  'Debugger-Version': string;
+  'Protocol-Version': string;
+  'User-Agent': string;
+  'V8-Version': string;
+  'WebKit-Version': string;
+  webSocketDebuggerUrl: string;
 };
 
-const route: HTTPRoute = {
-  accepts: [contentTypes.any],
-  auth: true,
-  browser: null,
-  concurrency: false,
-  contentTypes: [contentTypes.json],
-  description: `Returns a JSON payload that acts as a pass-through to the DevTools /json/version protocol in Chrome.`,
-  handler: async (req: Request, res: ServerResponse): Promise<void> => {
+export default class GetJSONVersion extends HTTPRoute {
+  accepts = [contentTypes.any];
+  auth = true;
+  browser = null;
+  concurrency = false;
+  contentTypes = [contentTypes.json];
+  description = `Returns a JSON payload that acts as a pass-through to the DevTools /json/version protocol in Chrome.`;
+  method = Methods.get;
+  path = HTTPManagementRoutes.jsonVersion;
+  tags = [APITags.management];
+
+  private cachedJSON: ResponseSchema | undefined;
+
+  handler = async (req: Request, res: ServerResponse): Promise<void> => {
     const baseUrl = req.parsed.host;
     const protocol = req.parsed.protocol.includes('s') ? 'wss' : 'ws';
 
-    const { _browserManager: browserManager } = route;
-
-    if (!browserManager) {
+    if (!this.browserManager()) {
       throw new util.BadRequest(`Couldn't load browsers running`);
     }
 
     try {
-      const response = {
-        ...(await browserManager().getVersionJSON()),
-        webSocketDebuggerUrl: `${protocol}://${baseUrl}`,
-      };
-      return util.jsonResponse(res, 200, response);
+      if (!this.cachedJSON) {
+        this.cachedJSON = {
+          ...(await this.browserManager().getVersionJSON()),
+          webSocketDebuggerUrl: `${protocol}://${baseUrl}`,
+        };
+      }
+
+      return util.jsonResponse(res, 200, this.cachedJSON);
     } catch (err) {
       return util.writeResponse(
         res,
@@ -52,10 +60,5 @@ const route: HTTPRoute = {
         contentTypes.text,
       );
     }
-  },
-  method: Methods.get,
-  path: HTTPManagementRoutes.jsonVersion,
-  tags: [APITags.management],
-};
-
-export default route;
+  };
+}
