08cfd0bee52389c849ab8289bfc7a6877d71946f
Joel Griffith <joel@joelgriffith.net>
2024-04-09 14:03:22 -0700

    Fix function calling when behind an external address

--- scripts/build-function.js
+++ scripts/build-function.js
@@ -1,18 +1,48 @@
 #!/usr/bin/env node
+/* eslint-disable no-undef */
+
 'use strict';
 
 import { build } from 'esbuild';
+import fs from 'fs/promises';
+import { join } from 'path';
 import { polyfillNode } from 'esbuild-plugin-polyfill-node';
 
-build({
-  bundle: true,
-  entryPoints: ['src/shared/utils/function/client.ts'],
-  outfile: 'static/function/client.js',
-  plugins: [
-    polyfillNode({
-      globals: {
-        process: false,
-      },
-    }),
-  ],
-});
+const html = (contents) => `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>browserless.io function runner</title>
+    <script type="module">
+    ${contents}
+    </script>
+  </head>
+  <body>
+  </body>
+</html>
+`;
+
+const entryPoints = ['src/shared/utils/function/client.ts'];
+const outfile = join(process.cwd(), 'static/function/client.js');
+const htmlLocation = join(process.cwd(), 'static/function/index.html');
+
+(async () => {
+  await build({
+    bundle: true,
+    entryPoints,
+    outfile,
+    plugins: [
+      polyfillNode({
+        globals: {
+          process: false,
+        },
+      }),
+    ],
+  });
+  const contents = await fs.readFile(outfile, 'utf-8');
+  const final = html(contents);
+
+  await fs.writeFile(htmlLocation, final);
+})();
