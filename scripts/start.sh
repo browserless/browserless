3ced82cbc5f388d65b3357c3c223ff26d57dc4e6
Joel Griffith <joel@joelgriffith.net>
2024-04-10 13:05:21 -0700

    Add support for overriding display for GPU support

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Setup env variables
-export DISPLAY=:99
-
 # When docker restarts, this file is still there,
 # so we need to kill it just in case
 [ -f /tmp/.X99-lock ] && rm -f /tmp/.X99-lock
@@ -16,8 +13,11 @@ _kill_procs() {
 # Relay quit commands to processes
 trap _kill_procs SIGTERM SIGINT
 
-Xvfb :99 -screen 0 1920x1080x16 -nolisten tcp -nolisten unix &
-xvfb=$!
+if [ -z "$DISPLAY" ] then
+  Xvfb :99 -screen 0 1024x768x16 -nolisten tcp -nolisten unix &
+  xvfb=$!
+  export DISPLAY=:99
+fi
 
 dumb-init -- node build/index.js $@ &
 node=$!
