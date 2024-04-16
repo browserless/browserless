c46481421fd4cabbe257e8c7cbde495bd88298df
Michael Tuttle <openam@gmail.com>
2024-04-16 09:48:28 -0600

    Fix scaffold to have logger as 3rd argument (#3998)
    * Fix scaffold to have logger as 3rd argument
    
    * Fix use logger.log in scaffold hello-world

--- bin/scaffold/src/hello-world.http.ts
+++ bin/scaffold/src/hello-world.http.ts
@@ -22,11 +22,8 @@ export default class HelloWorldHTTPRoute extends HTTPRoute {
   method = Methods.get;
   path = '/hello';
   tags = [APITags.management];
-  handler = async (
-    _req: Request,
-    _logger: Logger,
-    res: Response,
-  ): Promise<void> => {
+  handler = async (req: Request, res: Response, logger: Logger): Promise<void> => {
+    logger.verbose(`${req.method} /hello was called!`);
     const response: ResponseSchema = 'Hello World!';
     return writeResponse(res, 200, response, contentTypes.text);
   };
