f770a25efdb377c16a102238a3484bba7da81b21
Anderson Martínez <anderson@browserless.io>
2024-05-31 13:08:00 -0400

    test: remove unit test

--- src/routes/management/tests/management.spec.ts
+++ src/routes/management/tests/management.spec.ts
@@ -1,7 +1,5 @@
 import { Browserless, Config, Metrics } from '@browserless.io/browserless';
-import { CdpBrowser } from 'puppeteer-core/lib/esm/puppeteer';
 import { expect } from 'chai';
-import puppeteer from 'puppeteer-core';
 
 describe('Management APIs', function () {
   let browserless: Browserless;
@@ -82,35 +80,6 @@ describe('Management APIs', function () {
         expect(res.status).to.equal(200);
       },
     );
-  });
-
-  it('filters sessions by trackingId', async () => {
-    await start();
-    const browsers = (await Promise.all(
-      [1, 2, 3, 4, 5].map(
-        (i) =>
-          new Promise(async (resolve) => {
-            const browser = await puppeteer.connect({
-              browserWSEndpoint: `ws://localhost:3000/chromium?token=6R0W53R135510&trackingId=tracker${i}`,
-            });
-
-            resolve(browser);
-          }),
-      ),
-    )) as CdpBrowser[];
-
-    await fetch(
-      'http://localhost:3000/sessions?token=6R0W53R135510&trackingId=tracker1',
-    ).then(async (res) => {
-      const sessions = await res.json();
-      expect(sessions.length).to.equal(2); // 2 objects, one for the Browser, one for the Page
-      expect(res.headers.get('content-type')).to.equal(
-        'application/json; charset=UTF-8',
-      );
-      expect(res.status).to.equal(200);
-    });
-
-    browsers.forEach((b) => b.close());
   });
 
   it('allows requests to /active', async () => {
