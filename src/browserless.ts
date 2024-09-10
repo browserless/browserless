df56aed28c97dfbbf663b6ee7489f9ab76af5a51
Joel Griffith <joel@joelgriffith.net>
2024-09-10 11:55:30 -0700

    Logging installed browsers

--- src/browserless.ts
+++ src/browserless.ts
@@ -25,6 +25,7 @@ import {
   WebKitPlaywright,
   WebSocketRoute,
   availableBrowsers,
+  dedent,
   getRouteFiles,
   makeExternalURL,
   printLogo,
@@ -357,7 +358,8 @@ export class Browserless extends EventEmitter {
         !installedBrowsers.some((b) => b.name === route.browser?.name)
       ) {
         throw new Error(
-          `Couldn't load route "${route.path}" due to missing browser binary for "${route.browser?.name}"`,
+          dedent(`Couldn't load route "${route.path}" due to missing browser binary for "${route.browser?.name}".
+            Installed Browsers: ${installedBrowsers.join(', ')}`),
         );
       }
     });
