a1915d6edfa38e45fabb61dec23c19011cac8874
Joel Griffith <joel@joelgriffith.net>
2025-08-12 09:38:12 -0700

    Fix routes for /chromium/playwright

--- src/routes/chromium/ws/playwright.ts
+++ src/routes/chromium/ws/playwright.ts
@@ -1,17 +1,8 @@
 import {
-  BrowserlessRoutes,
-  ChromiumPlaywright,
-  WebsocketRoutes,
-} from '@browserless.io/browserless';
-
-import {
-  default as Playwright,
+  default as ChromiumPlaywrightWebSocketRoute,
   QuerySchema as SharedQuerySchema,
 } from '../../../shared/chromium.playwright.ws.js';
 
-export default class ChromiumPlaywrightWebSocketRoute extends Playwright {
-  name = BrowserlessRoutes.ChromiumPlaywrightWebSocketRoute;
-  browser = ChromiumPlaywright;
-  path = [WebsocketRoutes.chromiumPlaywright];
-}
+export default ChromiumPlaywrightWebSocketRoute;
+
 export type QuerySchema = SharedQuerySchema;
