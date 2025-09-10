9d2382decd4f63ed7cc73168ce0b91c5a1d74b38
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-09-10 13:42:47 +0200

    remove comments

--- scripts/install-adblock.js
+++ scripts/install-adblock.js
@@ -33,33 +33,22 @@ import fsExtra from 'fs-extra';
             .on('finish', resolve);
         }),
     );
-
-  // Delete existing uBlockLite folder if it exists
+  
   if (existsSync(uBlockLiteDir)) {
     await deleteAsync(uBlockLiteDir);
   }
 
-  // Fetch latest release info
   const data = await fetch(
     'https://api.github.com/repos/uBlockOrigin/uBOL-home/releases/latest'
   );
   const json = await data.json();
 
-  // Download ZIP
   await downloadUrlToDirectory(json.assets[0].browser_download_url, zipFile);
 
-  // Extract ZIP
   await unzip(zipFile, { dir: tmpDir });
 
-  // tmpDir is already the extension folder
-  if (!existsSync(join(tmpDir, 'manifest.json'))) {
-    throw new Error('manifest.json not found in extracted folder');
-  }
-
-  // Move folder to extensions directory
   await fsExtra.move(tmpDir, uBlockLiteDir, { overwrite: true });
-
-  // Clean up ZIP file
+  
   await deleteAsync(zipFile, { force: true }).catch((err) => {
     console.warn('Could not delete temporary download file: ' + err.message);
   });
